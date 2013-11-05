9fbdba927715108a0f151cd62233b09011d366a3
examplesproject <info@examplesproject.org.uk>
2013-11-05 15:11:00 +0000

    adds missing semi-colon to hello.js

--- hello.js
+++ hello.js
@@ -5,7 +5,7 @@ http.createServer(function(request, response) {
 	response.writeHead(200, {
 		'Content-Type': 'text/plain'
 	});
-	response.end('Hello World\n') // missing semi-colon will fail the build
+	response.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
